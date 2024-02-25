2110abb286c5310fb0435c9de1203739f3c22195
saipuli05 <156145913+saipuli05@users.noreply.github.com>
2024-02-25 02:13:07 +0000

    Commit Message goes here

--- src/hw2_main.c
+++ src/hw2_main.c
@@ -10,8 +10,27 @@
 #include <unistd.h> 
 
 int main(int argc, char **argv) {
+    int o;
+    int flag1 = 0;
+    int flag2 = 0;
+    while((o = getopt(argc, argv, "i:o:")) != -1){
+        switch(o){
+            case 'i':
+                flag1 = 1;
+                break;
+            case 'o':
+                flag2 = 1;
+                break;
+            default:
+            return UNRECOGNIZED_ARGUMENT;
+        }
+    if(!flag1 || !flag2){
+        return MISSING_ARGUMENT;
+    }
+    }
+    
     (void)argc;
     (void)argv;
 
-    return 1000;
+    return 0;
 }
