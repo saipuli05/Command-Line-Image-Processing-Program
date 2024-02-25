c3040f803e55144fae072f8d070a61a27f4337bd
saipuli05 <156145913+saipuli05@users.noreply.github.com>
2024-02-25 02:22:55 +0000

    Commit Message goes here

--- src/hw2_main.c
+++ src/hw2_main.c
@@ -13,7 +13,10 @@ int main(int argc, char **argv) {
     int o;
     int flag1 = 0;
     int flag2 = 0;
-    while((o = getopt(argc, argv, "i:o:")) != -1){
+    int flag3 = 0;
+    int flag4 = 0;
+    int flag5 = 0;
+    while((o = getopt(argc, argv, "i:o:c:p:r")) != -1){
         switch(o){
             case 'i':
                 flag1 = 1;
@@ -21,10 +24,19 @@ int main(int argc, char **argv) {
             case 'o':
                 flag2 = 1;
                 break;
+            case 'c':
+                flag3 = 1;
+                break;
+            case 'p':
+                flag4 = 1;
+                break;
+            case 'r':
+                flag5 = 1;
+                break;
             default:
             return UNRECOGNIZED_ARGUMENT;
         }
-    if(!flag1 || !flag2){
+    if(!flag1 || !flag2 || !flag3 || !flag4 || !flag5){
         return MISSING_ARGUMENT;
     }
     }
